c08e671385e2de5ff1ff6c9829d4df7c44a7f267
Cristian  CC <cristi.nord@gmail.com>
2018-10-20 22:58:20 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -6,8 +6,9 @@
 # suite_e: "Crumpet the Elf"
 # }
 
-def select_winner(hash)
-  hash.each do |suite, name|
+def select_winner(passengers)
+  winner = ""
+passengers.each do |suite, name|
   if suite == :suite_a && name.start_with?("A")
     winner = name
   end
